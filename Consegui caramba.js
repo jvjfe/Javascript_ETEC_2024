d6d6cda7f7c3301f0ab4e5f11841b47d617e16e2
Jvjfe <82617361+jvjfe@users.noreply.github.com>
2021-06-23 14:29:52 -0300

    Add files via upload

---
+++ Consegui caramba.js
@@ -0,0 +1,11 @@
+ function retornaInvertido(valorString) {
+    return valorString.split('').reverse().join('');
+ }
+
+
+ 
+ var a = retornaInvertido('Alexandre Ferri');
+
+
+console.log(a)
+//vaorString
